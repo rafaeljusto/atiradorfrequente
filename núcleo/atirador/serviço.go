d904007c321f5fe2229a36a56a787961f4424fc3
Rafael Dantas Justo <rafael@registro.br>
2016-06-15 11:04:29 -0300

    Adiciona tarefas a serem executadas no serviço
    O serviço é responsável por persistir a informação e gerar a imagem com o
    número de controle.

--- núcleo/atirador/serviço.go
+++ núcleo/atirador/serviço.go
@@ -15,9 +15,12 @@ type serviço struct {
 }
 
 func (s serviço) CadastrarFrequência(frequênciaPedidoCompleta protocolo.FrequênciaPedidoCompleta) (protocolo.FrequênciaResposta, error) {
+	// TODO(rafaeljusto): Persistir na base de dados
+	// TODO(rafaeljusto): Criar imagem com o número de controle
 	return protocolo.FrequênciaResposta{}, nil
 }
 
 func (s serviço) ConfirmarFrequência(frequênciaConfirmaçãoPedidoCompleta protocolo.FrequênciaConfirmaçãoPedidoCompleta) error {
+	// TODO(rafaeljusto): Persistir imagem de confirmação
 	return nil
 }
