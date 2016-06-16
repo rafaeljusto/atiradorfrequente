b34b0c6fa3e8d075dece870a71e81a45c08cd544
Rafael Dantas Justo <rafael@registro.br>
2016-06-16 09:55:13 -0300

    Testa as rotas registradas para o servidor REST

---
+++ rest/handler/registro_test.go
@@ -0,0 +1,25 @@
+package handler_test
+
+import (
+	"testing"
+
+	"github.com/rafaeljusto/atiradorfrequente/rest/handler"
+)
+
+func TestRotas(t *testing.T) {
+	if handler.Rotas == nil {
+		t.Errorf("As rotas do servidor REST estão vazias")
+	}
+
+	if h, ok := handler.Rotas["/frequencia/{cr}"]; !ok {
+		t.Error("Handler de cadastro da frequência do atirador não encontrado")
+	} else if h() == nil {
+		t.Error("Handler de cadastro da frequência do atirador corrompido")
+	}
+
+	if h, ok := handler.Rotas["/frequencia/{cr}/{numeroControle}"]; !ok {
+		t.Error("Handler de confirmação da frequência do atirador não encontrado")
+	} else if h() == nil {
+		t.Error("Handler de confirmação da frequência do atirador corrompido")
+	}
+}
