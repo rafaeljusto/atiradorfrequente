b780b1b3239fd6b9ee9b8539b1c5e94ecfe05967
Rafael Dantas Justo <rafael@registro.br>
2016-09-19 10:29:13 -0300

    Simplificate a execução dos containers
    Não é necessário mapear os logs do container de banco de dados. E para tornar a
    distribuição mais limpa estamos copiando a estrutura SQL para o diretório
    temporário.

--- instalação/docker/psql/entrypoint.sh
+++ instalação/docker/psql/entrypoint.sh
@@ -7,6 +7,6 @@ createuser --username "$POSTGRES_USER" atiradorfrequente
 
 psql --username "$POSTGRES_USER" -c "ALTER USER atiradorfrequente WITH PASSWORD '$POSTGRES_PASSWORD';"
 psql --username "$POSTGRES_USER" -c "GRANT CONNECT ON DATABASE atiradorfrequente to atiradorfrequente;"
-psql --username "$POSTGRES_USER" atiradorfrequente < ../atiradorfrequente.sql
+psql --username "$POSTGRES_USER" atiradorfrequente < /tmp/atiradorfrequente.sql
 psql --username "$POSTGRES_USER" -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO atiradorfrequente;" atiradorfrequente
 psql --username "$POSTGRES_USER" -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO atiradorfrequente;" atiradorfrequente
